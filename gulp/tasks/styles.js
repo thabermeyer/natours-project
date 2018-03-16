3c5ed340563193745e967a1848ce365bdefe2d2f
Trevor Habermeyer <thabermeyer@gmail.com>
2018-03-16 17:47:28 -0600

    Organizes CSS; fixed gulp-sass bug; working on header

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,20 +1,9 @@
 var gulp = require('gulp');
-var autoprefixer = require('autoprefixer');
-
-/*
-var postcss = require('gulp-postcss');
-var cssvars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
-var cssImport = require('postcss-import');
-var mixins = require('postcss-mixins');
-*/
+var sass = require('gulp-sass');
+// var autoprefixer = require('autoprefixer');
 
 gulp.task('styles', function() {
-  return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([autoprefixer]))
-    .on('error', function(errorInfo) {
-      console.log(errorInfo.toString());
-      this.emit('end');
-    })
+  return gulp.src('./app/assets/styles/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./app/temp/styles'));
 });
